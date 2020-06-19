4b2aa5f21c28306d56d6c1b2173865ca7d6e36da
Brian Russell <bruss@brians-mbp.lan>
2020-06-19 12:20:00 -0400

    Got all endpoints working in postman

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,24 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
+const constants = require('../config/constants.js')
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = constants.jwtSecret;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+        if (err) {
+          console.log(err)
+          res.status(201).json({message: 'Invalid Token'})
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
---
+++ config/constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'his name is Robert Paulson'
+}
